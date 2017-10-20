4a970cd977803f09860d7dc5422c444f29683d5c
effieMoran <eliana_474@hotmail.com>
2017-10-20 00:59:52 -0300

    carga de imagenes

--- src/main/java/com/metodologia/sistemas/App.java
+++ src/main/java/com/metodologia/sistemas/App.java
@@ -1,9 +1,12 @@
 package com.metodologia.sistemas;
 
+import com.metodologia.sistemas.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -12,5 +15,13 @@ public class App
 {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 }
--- src/main/java/com/metodologia/sistemas/entity/Pago.java
+++ src/main/java/com/metodologia/sistemas/entity/Pago.java
@@ -39,7 +39,7 @@ public class Pago {
     @Getter
     @Setter
     private MedioDePago medioDePago;
-    
+
     @NotNull
     @Temporal(TemporalType.DATE)
     @Getter
